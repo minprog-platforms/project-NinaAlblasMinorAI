a120a93d32e31fae3ddeb0023da0acc98875ab40
Nina <ninamalblas@gmail.com>
2022-05-18 16:15:42 +0200

    hi

---
+++ TipsyTimer/Shared/StoredData.swift
@@ -0,0 +1,20 @@
+//
+//  TipsyData.swift
+//  TipsyTimer
+//
+//  Created by Nina Alblas on 12/05/2022.
+//
+
+import Foundation
+
+struct TipsyData {
+    // sla hier de door de user ingevoerde waarden op zodat als hij een scherm teruggaat, de waarde nog staat
+    var gender: Gender?
+    var age: Double?
+    var weight: Double?
+    var height: Double?
+    var alcoholConsumption: Double?
+    var drinkingTime: Double?
+    var drivingExperience: DrivingExperience?
+}
+
